4e86b1475f87ade98d20c6bafe25b0189668905f
Casey Schadewitz <schadewitzcasey@gmail.com>
2022-03-20 20:10:30 -0700

    Add controller generation

---
+++ Generators/Api/ControllerGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Devise.Utilities;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devise.Generators.Api
+{
+    public static class ControllerGenerator
+    {
+        public static void GenerateCottle(GeneratorExecutionContext context, DeviseConfig config, IEnumerable<ClassDeclarationSyntax> devisableEntities)
+        {
+            if (devisableEntities is null)
+                throw new ArgumentNullException(nameof(devisableEntities));
+            foreach (ClassDeclarationSyntax entity in devisableEntities)
+            {
+                if (!entity.Parent.IsKind(SyntaxKind.NamespaceDeclaration))
+                {
+                    //Throw Error to user that subclasses are not supported
+                }
+                string renderedCode = CottleRenderer.Render(
+                    TemplateResourceReader.ReadTemplate("controller"),
+                    SyntaxParser.GetEntityCottleContext(config, entity)
+                    );
+
+                context.AddSource($"{entity.Identifier.Text}Controller.g.cs", SourceText.From(renderedCode, Encoding.UTF8));
+            }
+
+        }
+    }
+}
---
+++ Generators/Business/RepositoryInterfaceGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Devise.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devise.Generators.Business
+{
+    public static class RepositoryInterefaceGenerator
+    {
+        //Devise Attribute Definition
+
+        public static void GenerateCottle(GeneratorExecutionContext context, DeviseConfig config, IEnumerable<ClassDeclarationSyntax> devisableEntities)
+        {
+            if (devisableEntities is null)
+                throw new ArgumentNullException(nameof(devisableEntities));
+            string renderedCode = CottleRenderer.Render(
+                TemplateResourceReader.ReadTemplate("irepository"),
+                SyntaxParser.GetMappingCottleContext(config, devisableEntities)
+                );
+
+            context.AddSource($"IRepository.g.cs", SourceText.From(renderedCode, Encoding.UTF8));
+
+        }
+    }
+}
+
